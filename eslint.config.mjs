04e23c224be7c17850bad2bf0a0a02b19858ed19
drizzle <drizzlemisty@users.noreply.github.com>
2025-03-11 15:43:20 +0800

    remove worker

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,34 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default tseslint.config({
+    extends: [
+        js.configs.recommended,
+        // ...tseslint.configs.recommended,
+        ...tseslint.configs.strict,
+        ...tseslint.configs.stylistic,
+        ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    ],
+    files: ['src/**/*.{ts,tsx}'],
+    ignores: ['dist'],
+    languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+    },
+    plugins: {
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+    },
+    rules: {
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+});
